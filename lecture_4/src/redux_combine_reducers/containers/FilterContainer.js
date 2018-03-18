Gtopuria/interactive_frontend_development_2017

Add redux combineReducers example

lecture_4/redux_combine_reducers/src/containers/FilterContainer.js
@@ -1,0 +1,17 @@
+import {connect} from 'react-redux';
+import {filterSet} from '../actions';
+
+import Filter from '../components/Filter';
+
+const mapStateToProps = (state, ownProps) => ({
+  // `ownProps` can be used to access properties that were provided to the
+  // container component
+  filterValue: state.filter,
+  text: ownProps.text
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onChange: (author) => dispatch(filterSet(author)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Filter);

urmastalimaa/interactive_frontend_development_2018

lecture_4/src/redux_combine_reducers/containers/FilterContainer.js
@@ -1,0 +1,17 @@
+import {connect} from 'react-redux';
+import {filterSet} from '../actions';
+
+import Filter from '../components/Filter';
+
+const mapStateToProps = (state, ownProps) => ({
+  // `ownProps` can be used to access properties that were provided to the
+  // container component
+  filterValue: state.filter,
+  text: ownProps.text
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onChange: (filter) => dispatch(filterSet(filter)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Filter);