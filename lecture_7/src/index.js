f8ba3aa85110eabd668d3b6c327386c5751e1216
urmastalimaa <urmas7@gmail.com>
2018-05-03 15:44:36 +0300

    Add connected React Router example
    React router connected to Redux store.

--- lecture_7/src/index.js
+++ lecture_7/src/index.js
@@ -1,4 +1,11 @@
+import {start as startConnectedReactRouterExample}
+  from './connected-react-router/ConnectedReactRouterExample';
+
 import {start as startRouterBasicsExample}
   from './router-basics/RouterBasicsExample';
 
-startRouterBasicsExample();
+if (document.location.search.match(/progress=connected_react_router/)) {
+  startConnectedReactRouterExample();
+} else {
+  startRouterBasicsExample();
+}
